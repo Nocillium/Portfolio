eeb9b2c3fb06c5c5b9d562a2f79b0a65f7a41a55
Chester Cruz <chester@clavionlabs.com>
2026-08-09 04:45:06 +0800

    Update portfolio with current changes

--- config.example.js
+++
@@ -1,9 +0,0 @@
-// EmailJS Configuration Template
-// Copy this file to config.js and add your credentials
-// config.js will be ignored by git (see .gitignore)
-
-const EMAILJS_CONFIG = {
-  PUBLIC_KEY: 'YOUR_PUBLIC_KEY_HERE',
-  SERVICE_ID: 'YOUR_SERVICE_ID_HERE',
-  TEMPLATE_ID: 'YOUR_TEMPLATE_ID_HERE'
-};
